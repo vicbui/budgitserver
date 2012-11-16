1880e6152008186145951c1377ef23343d6731e5
Vic Bui <quangvu88@hotmail.com>
2012-11-16 12:03:02 +0800

    started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BudgitServer::Application.config.secret_token = 'a2d914916b23c1acfbf9fcc82350eb934ffd3027a1a2f8c60cf00ba3f97fd2fec27853acd5f9c02116abebd177e4113771957d6d1446fccfb290377f3b80e550'
